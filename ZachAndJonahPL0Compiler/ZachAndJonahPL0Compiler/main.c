e26d34a641ae9b6dfd2396bd728e459735bdfe19
z-chenet <z.chenet@gmail.com>
2014-10-07 17:27:54 -0400

    initial commit

---
+++ ZachAndJonahPL0Compiler/ZachAndJonahPL0Compiler/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  ZachAndJonahPL0Compiler
+//
+//  Created by Zachary Chenet on 10/7/14.
+//  Copyright (c) 2014 Zachary Chenet. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
