d58f3f68a971ef7a1da80d430d85fdd2fbb08f20
Jonah Katz <jak1192@gmail.com>
2014-10-13 19:58:14 -0400

    IntermediateCodeGenerator file (separate from Parser.c, as stated in the hw doc)

---
+++ ZachAndJonahPL0Compiler/ZachAndJonahPL0Compiler/IntermediateCodeGenerator.c
@@ -0,0 +1,15 @@
+
+/*********
+ 
+ Jonah Katz and Zachary Chenet
+ 
+ University of Central Florida
+ COP 3402: System Software
+ Fall 2014
+ 
+ Homework #3 (Parser - Code Generator)
+ 
+ *********/
+
+
+#include "IntermediateCodeGenerator.h"
---
+++ ZachAndJonahPL0Compiler/ZachAndJonahPL0Compiler/IntermediateCodeGenerator.h
@@ -0,0 +1,21 @@
+
+/*********
+ 
+ Jonah Katz and Zachary Chenet
+ 
+ University of Central Florida
+ COP 3402: System Software
+ Fall 2014
+ 
+ Homework #3 (Parser - Code Generator)
+ 
+ *********/
+
+
+
+#ifndef __ZachAndJonahPL0Compiler__IntermediateCodeGenerator__
+#define __ZachAndJonahPL0Compiler__IntermediateCodeGenerator__
+
+#include <stdio.h>
+
+#endif /* defined(__ZachAndJonahPL0Compiler__IntermediateCodeGenerator__) */
