3c821c6d0cf2a490b70415e2883520402fbb698c
z-chenet <z.chenet@gmail.com>
2014-10-13 21:44:52 -0400

    commits that i have to do merge

--- ZachAndJonahPL0Compiler/ZachAndJonahPL0Compiler/Parser.c
+++ ZachAndJonahPL0Compiler/ZachAndJonahPL0Compiler/Parser.c
@@ -6,4 +6,8 @@
 //  Copyright (c) 2014 Zachary Chenet. All rights reserved.
 //
 
+
+/***************************************************************************/
+/*                       Procedure and call not supported                  */
+//  after checking that program ends with perioid call HALT
 #include <stdio.h>
